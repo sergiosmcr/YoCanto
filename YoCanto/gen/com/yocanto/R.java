5398801d8d39db1d8cb3e3859303df1c6bbc7549
Sergio Salazar <sergiosmcr@gmail.com>
2012-10-11 03:16:49 -0600

    Ahora el app tiene spinners

--- YoCanto/gen/com/yocanto/R.java
+++ YoCanto/gen/com/yocanto/R.java
@@ -20,7 +20,16 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int menu_settings=0x7f080000;
+        public static final int TableLayout1=0x7f080000;
+        public static final int menu_settings=0x7f080009;
+        public static final int spinner1=0x7f080003;
+        public static final int spinner2=0x7f080004;
+        public static final int spinner3=0x7f080007;
+        public static final int spinner4=0x7f080008;
+        public static final int tableRow1=0x7f080001;
+        public static final int tableRow2=0x7f080005;
+        public static final int textView1=0x7f080002;
+        public static final int textView2=0x7f080006;
     }
     public static final class layout {
         public static final int activity_yo_canto=0x7f030000;
@@ -33,6 +42,8 @@ public final class R {
         public static final int hello_world=0x7f050001;
         public static final int menu_settings=0x7f050002;
         public static final int title_activity_yo_canto=0x7f050003;
+        public static final int tv_entrada=0x7f050004;
+        public static final int tv_gloria=0x7f050005;
     }
     public static final class style {
         public static final int AppTheme=0x7f060000;
--- YoCanto/src/com/yocanto/YoCantoActivity.java
+++ YoCanto/src/com/yocanto/YoCantoActivity.java
@@ -1,18 +1,73 @@
 package com.yocanto;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
-import android.view.MenuItem;
-import android.support.v4.app.NavUtils;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class YoCantoActivity extends Activity {
 
+	protected ArrayAdapter<String> mAdapterQue;
+	protected ArrayAdapter<String> mAdapterQuien;
+	
+	String cantos[] = {
+			"Bendita eres tú María", 
+			"Benedictus",
+			"Zaqueo",
+			"Eres Hermoso"
+	};
+    String cantores[] = {
+    		"Fulano",
+    		"Mengano",
+    		"Perensejo",
+    		"Kiko Argüello"
+    };
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_yo_canto);
+        
+        
+        Spinner spEntradaQue = (Spinner) findViewById(R.id.spinner1);
+        Spinner spEntradaQuien = (Spinner) findViewById(R.id.spinner2);
+        Spinner spGloriaQue = (Spinner) findViewById(R.id.spinner3);
+        Spinner spGloriaQuien = (Spinner) findViewById(R.id.spinner4);
+        
+        
+        this.mAdapterQue = new ArrayAdapter<String>(this, 
+        		android.R.layout.simple_spinner_dropdown_item, cantos);
+        
+        spEntradaQue.setAdapter(mAdapterQue);
+        spEntradaQue.setOnItemSelectedListener(spEntradaQueListener);
+        
+        this.mAdapterQuien = new ArrayAdapter<String>(this, 
+        		android.R.layout.simple_spinner_dropdown_item, cantores);
+        
+        spEntradaQuien.setAdapter(mAdapterQuien);
+        
+        
     }
+    
+    OnItemSelectedListener spEntradaQueListener = new OnItemSelectedListener() {
+
+		public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2,
+				long arg3) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		public void onNothingSelected(AdapterView<?> arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+	}; {
+    	
+	};
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
